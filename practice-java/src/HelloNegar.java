fb69db3b8dd0ac6d908eb5cc6639256465005561
Negar Lashgari <negar.lashgari@gmail.com>
2023-05-09 14:26:01 -0400

    adding second class

---
+++ practice-java/src/HelloNegar.java
@@ -0,0 +1,8 @@
+public class HelloNegar {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        System.out.println("Now that we have a JDK installed, we are ready to create our first program");
+        System.out.println("We will use a text editor for this process and then run the completed program");
+        System.out.println("Edit the program so that it prints out your name.");
+    }
+}
